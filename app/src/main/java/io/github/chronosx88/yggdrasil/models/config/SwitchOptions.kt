ef457d7a9c03ce7ceedc0c6808bbcc8cde9570c1
ChronosX88 <chronosx88@gmail.com>
2019-08-25 14:57:50 +0400

    fix: Change all Config model properties to var instead of val

--- app/src/main/java/io/github/chronosx88/yggdrasil/models/config/SwitchOptions.kt
+++ app/src/main/java/io/github/chronosx88/yggdrasil/models/config/SwitchOptions.kt
@@ -3,5 +3,5 @@ package io.github.chronosx88.yggdrasil.models.config
 import com.google.gson.annotations.SerializedName
 
 data class SwitchOptions (
-	@SerializedName("maxTotalQueueSize") val maxTotalQueueSize : Int // Maximum size of all switch queues combined (in bytes).
+	@SerializedName("maxTotalQueueSize") var maxTotalQueueSize : Int // Maximum size of all switch queues combined (in bytes).
 )
