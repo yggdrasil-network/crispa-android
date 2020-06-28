767079cfdc8fae6ed338fe19dbc6047425d1a7ea
vadym <vadym.vikulin@gmail.com>
2020-06-28 13:39:18 -0700

    1. ipv6 family possible fix

--- app/src/main/java/io/github/chronosx88/yggdrasil/YggdrasilTunService.kt
+++ app/src/main/java/io/github/chronosx88/yggdrasil/YggdrasilTunService.kt
@@ -65,6 +65,7 @@ class YggdrasilTunService : VpnService() {
         var builder = Builder()
             .addAddress(address, 7)
             .allowFamily(OsConstants.AF_INET)
+            .allowFamily(OsConstants.AF_INET6)
             .setMtu(MAX_PACKET_SIZE)
         if (dns.size > 0) {
             for (d in dns) {
