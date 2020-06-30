c736c06f51fe5ea1037b537d32817bf88cf227fb
vadym <vadym.vikulin@gmail.com>
2020-06-30 07:14:11 -0700

    static IP fix, fixed dynamic peers list crash, peer/DNS list layout fix

--- app/src/main/java/io/github/chronosx88/yggdrasil/PeerListActivity.kt
+++ app/src/main/java/io/github/chronosx88/yggdrasil/PeerListActivity.kt
@@ -3,12 +3,10 @@ package io.github.chronosx88.yggdrasil
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.ListView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
