6f311b7213cd34f12a7714433f86318c938ae3f9
vadym <vadym.vikulin@gmail.com>
2020-08-05 14:48:57 -0700

    1. condition fix

--- app/src/main/java/io/github/chronosx88/yggdrasil/YggdrasilTunService.kt
+++ app/src/main/java/io/github/chronosx88/yggdrasil/YggdrasilTunService.kt
@@ -216,9 +216,7 @@ class YggdrasilTunService : VpnService() {
         try {
             // Read the outgoing packet from the input stream.
             val length = tunInputStream.read(buffer)
-            if(length>0) {
-                yggConduitEndpoint.send(buffer.sliceArray(IntRange(0, length - 1)))
-            }
+            yggConduitEndpoint.send(buffer.sliceArray(IntRange(0, length - 1)))
         } catch (e: IOException) {
             e.printStackTrace()
         }
