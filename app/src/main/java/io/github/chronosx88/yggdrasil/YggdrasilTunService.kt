a2a50ce2f0e5e591ff1fb40c9b297bbbd4d00e79
vadym <vadym.vikulin@gmail.com>
2020-08-07 00:44:20 -0700

    1. ipv4 route fix

--- app/src/main/java/io/github/chronosx88/yggdrasil/YggdrasilTunService.kt
+++ app/src/main/java/io/github/chronosx88/yggdrasil/YggdrasilTunService.kt
@@ -88,6 +88,7 @@ class YggdrasilTunService : VpnService() {
         } else {
             Builder()
                 .addAddress(address, 7)
+                .addAddress("1.1.1.1", 32)
                 .addRoute("200::", 7)
                 .setMtu(MAX_PACKET_SIZE)
         }
