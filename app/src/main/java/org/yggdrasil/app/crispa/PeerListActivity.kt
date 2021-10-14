ae31223428030a048510569c9459f8085082261a
vadym <vadym.vikulin@gmail.com>
2021-10-14 17:23:44 +0300

    1. Seted up extras using Yggdrasil develop branch 2. Fixed https://github.com/yggdrasil-network/yggdrasil-extras/issues/15 3. Used Neils peers list by default

--- app/src/main/java/org/yggdrasil/app/crispa/PeerListActivity.kt
+++ app/src/main/java/org/yggdrasil/app/crispa/PeerListActivity.kt
@@ -43,7 +43,7 @@ class PeerListActivity : AppCompatActivity() {
 
     companion object {
         const val PEER_LIST = "PEER_LIST"
-        const val PEER_LIST_URL = "http://y4.rivchain.org/rest/peers.json"
+        const val PEER_LIST_URL = "https://publicpeers.neilalexander.dev/publicnodes.json"
         const val CACHE_NAME = "PEER_LIST_CACHE"
         const val ONLINE_PEERINFO_LIST = "online_peer_info_list"
         const val OFFLINE_PEERINFO_LIST = "offline_peer_info_list"
